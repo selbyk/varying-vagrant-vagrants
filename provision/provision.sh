f15acc294ac5e18906430b91913d84d68cb5b724
Selby Kendrick <selby.kendrick@gmail.com>
2013-06-25 23:35:44 -0500

    Think she's working

--- provision/provision.sh
+++ provision/provision.sh
@@ -8,192 +8,6 @@
 # We calculate the duration of provisioning at the end of this script.
 start_seconds=`date +%s`
 
-# PACKAGE INSTALLATION
-#
-# Build a bash array to pass all of the packages we want to install to a single
-# apt-get command. This avoids having to do all the leg work each time a
-# package is set to install. It also allows us to easily comment out or add
-# single packages. We set the array as empty to begin with so that we can
-# append individual packages to it as required.
-apt_package_install_list=()
-
-# Start with a bash array containing all of the packages that we want to 
-# install on the system. We'll then loop through each of these and check
-# individual status before passing to the apt_package_install_list array.
-apt_package_check_list=(
-
-	# PHP5
-	#
-	# Our base packages for php5. As long as php5-fpm and php5-cli are
-	# installed, there is no need to install the general php5 package, which
-	# can sometimes install apache as a requirement.
-	php5-fpm
-	php5-cli
-
-	# Common and dev packages for php
-	php5-common
-	php5-dev
-
-	# Extra PHP modules that we find useful
-	php5-memcache
-	php5-imagick
-	php5-xdebug
-	php5-mcrypt
-	php5-mysql
-	php5-imap
-	php5-curl
-	php-pear
-	php5-gd
-	php-apc
-
-	# nginx is installed as the default web server
-	nginx
-
-	# memcached is made available for object caching
-	memcached
-
-	# other packages that come in handy
-	imagemagick
-	subversion
-	git-core
-	unzip
-	ngrep
-	curl
-	make
-	vim
-
-	# dos2unix
-	# Allows conversion of DOS style line endings to something we'll have less
-	# trouble with in Linux.
-	dos2unix
-)
-
-echo "Check for packages to install..."
-
-# Loop through each of our packages that should be installed on the system. If
-# not yet installed, it should be added to the array of packages to install.
-for pkg in "${apt_package_check_list[@]}"
-do
-	if dpkg -s $pkg | grep -q 'Status: install ok installed';
-	then 
-		echo $pkg already installed
-	else
-		echo $pkg not yet installed
-		apt_package_install_list+=($pkg)
-	fi
-done
-
-# MySQL
-#
-# The current state of MySQL should be done outside of the looping done above.
-# This allows us to set the MySQL specific settings for the root password
-# so that provisioning does not require any user input.
-if dpkg -s mysql-server | grep -q 'Status: install ok installed';
-then
-	echo "mysql-server already installed"
-else 
-	# We need to set the selections to automatically fill the password prompt
-	# for mysql while it is being installed. The password in the following two
-	# lines *is* actually set to the word 'blank' for the root user.
-	echo mysql-server mysql-server/root_password password blank | debconf-set-selections
-	echo mysql-server mysql-server/root_password_again password blank | debconf-set-selections
-	apt_package_install_list+=('mysql-server')
-fi
-
-# Provide our custom apt sources before running `apt-get update`
-ln -sf /srv/config/apt-source-append.list /etc/apt/sources.list.d/vvv-sources.list | echo "Linked custom apt sources"
-
-# If there are any packages to be installed in the apt_package_list array,
-# then we'll run `apt-get update` and then `apt-get install` to proceed.
-if [ ${#apt_package_install_list[@]} = 0 ];
-then 
-	printf "No packages to install.\n\n"
-else
-	# Before running `apt-get update`, we should add the public keys for
-	# the packages that we are installing from non standard sources via
-	# our appended apt source.list
-
-	# Nginx.org nginx key ABF5BD827BD9BF62
-	gpg -q --keyserver keyserver.ubuntu.com --recv-key ABF5BD827BD9BF62
-	gpg -q -a --export ABF5BD827BD9BF62 | apt-key add -
-
-	# Launchpad Subversion key EAA903E3A2F4C039
-	gpg -q --keyserver keyserver.ubuntu.com --recv-key EAA903E3A2F4C039
-	gpg -q -a --export EAA903E3A2F4C039 | apt-key add -
-
-	# Launchpad PHP key 4F4EA0AAE5267A6C
-	gpg -q --keyserver keyserver.ubuntu.com --recv-key 4F4EA0AAE5267A6C
-	gpg -q -a --export 4F4EA0AAE5267A6C | apt-key add -
-
-	# Launchpad git key A1715D88E1DF1F24
-	gpg -q --keyserver keyserver.ubuntu.com --recv-key A1715D88E1DF1F24
-	gpg -q -a --export A1715D88E1DF1F24 | apt-key add -
-
-	# update all of the package references before installing anything
-	printf "Running apt-get update....\n"
-	apt-get update --assume-yes
-
-	# install required packages
-	printf "Installing apt-get packages...\n"
-	apt-get install --assume-yes ${apt_package_install_list[@]}
-
-	# Clean up apt caches
-	apt-get clean			
-fi
-
-# ack-grep
-#
-# Install ack-rep directory from the version hosted at beyondgrep.com as the
-# PPAs for Ubuntu Precise are not available yet.
-if [ -f /usr/bin/ack ]
-then
-	echo "ack-grep already installed"
-else
-	echo "Installing ack-grep as ack"
-	curl -s http://beyondgrep.com/ack-2.04-single-file > /usr/bin/ack && chmod +x /usr/bin/ack
-fi
-
-# COMPOSER
-#
-# Install or Update Composer based on current state. Updates are direct from
-# master branch on GitHub repository.
-if composer --version | grep -q 'Composer version';
-then
-	printf "Updating Composer...\n"
-	composer self-update
-else
-	printf "Installing Composer...\n"
-	curl -sS https://getcomposer.org/installer | php
-	chmod +x composer.phar
-	mv composer.phar /usr/local/bin/composer
-fi
-
-# PHPUnit
-#
-# Check that PHPUnit, Mockery, and Hamcrest are all successfully installed. If
-# not, then Composer should be given another shot at it. Versions for these
-# packages are controlled in the `/srv/config/phpunit-composer.json` file.
-if [ ! -d /usr/local/src/vvv-phpunit ]
-then
-	printf "Installing PHPUnit, Hamcrest and Mockery...\n"
-	mkdir -p /usr/local/src/vvv-phpunit
-	cp /srv/config/phpunit-composer.json /usr/local/src/vvv-phpunit/composer.json
-	sh -c "cd /usr/local/src/vvv-phpunit && composer install"
-else
-	cd /usr/local/src/vvv-phpunit
-	if composer show -i | grep -q 'mockery'; then echo 'Mockery installed';else vvvphpunit_update=1;fi
-	if composer show -i | grep -q 'phpunit'; then echo 'PHPUnit installed'; else vvvphpunit_update=1;fi
-	if composer show -i | grep -q 'hamcrest'; then echo 'Hamcrest installed'; else vvvphpunit_update=1;fi
-	cd ~/
-fi
-
-if [ "$vvvphpunit_update" = 1 ]
-then
-	printf "Update PHPUnit, Hamcrest and Mockery...\n"
-	cp /srv/config/phpunit-composer.json /usr/local/src/vvv-phpunit/composer.json
-	sh -c "cd /usr/local/src/vvv-phpunit && composer update"
-fi
-
 # SYMLINK HOST FILES
 printf "\nLink Directories...\n"
 
@@ -208,7 +22,6 @@ ln -sf /srv/config/php5-fpm-config/www.conf /etc/php5/fpm/pool.d/www.conf | echo
 ln -sf /srv/config/php5-fpm-config/php-custom.ini /etc/php5/fpm/conf.d/php-custom.ini | echo "Linked php-custom.ini to /etc/php5/fpm/conf.d/php-custom.ini"
 
 # Configuration for Xdebug - Mod disabled by default
-php5dismod xdebug
 ln -sf /srv/config/php5-fpm-config/xdebug.ini /etc/php5/fpm/conf.d/xdebug.ini | echo "Linked xdebug.ini to /etc/php5/fpm/conf.d/xdebug.ini"
 
 # Configuration for APC
@@ -278,10 +91,7 @@ sudo mysql -s -uroot -pblank -e 'GRANT ALL PRIVILEGES ON spare_hanger.* \
   sudo mysql -s -uroot -pblank -e 'GRANT ALL PRIVILEGES ON spare_hanger.* \
   TO "spare_hanger"@"any" IDENTIFIED BY "SecurePassword42";'
 sudo mysql -s -uspare_hanger -pSecurePassword42 -h localhost \
-  spare_hanger < /vagrant/www/sparehanger/sparehanger/protected/data/spare_hanger.sql
-
-sudo chown vagrant:vagrant /var/lib/phpmyadmin/tmp
-sudo cp /vagrant/config/php.ini /etc/php5/apache2/php.ini
+  spare_hanger < /srv/www/sparehanger/sparehanger/protected/data/spare_hanger.sql
 
 
 # Download phpMyAdmin 4.0.3
